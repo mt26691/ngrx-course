a592cf8d1ea371ed39b7fa2a335443a893bd89ca
Tung Nguyen <nguyenmanhtung848@gmail.com>
2020-12-20 09:38:58 +0700

    add dev tool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,6 +61,10 @@ const routes: Routes = [
         strictStateSerializability: true
       }
     }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   bootstrap: [AppComponent]
 })
