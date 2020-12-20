cc713705b9e325b907469a8665ee542f418f9d9b
Tung Nguyen <nguyenmanhtung848@gmail.com>
2020-12-20 11:37:00 +0700

    Auth State

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,19 +1,29 @@
+import { state } from '@angular/animations';
 import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  on
 } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
 
-export interface State {
-
+export interface AuthState {
+  user: User
 }
 
-export const reducers: ActionReducerMap<State> = {
-
-};
+export const initialAuthState: AuthState = { user: undefined };
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return { user: action .user}
+  })
+);
+
+export const metaReducers: MetaReducer<AuthState>[] = !environment.production ? [] : [];
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,12 +1,13 @@
 import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
+    ActionReducer,
+    ActionReducerMap,
+    createFeatureSelector,
+    createSelector,
+    MetaReducer,
+    State
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-import {routerReducer} from '@ngrx/router-store';
+import { routerReducer } from '@ngrx/router-store';
 
 export interface AppState {
 
@@ -16,7 +17,11 @@ export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer
 };
 
-export function logger(reducer:ActionReducer<any>)
+
+
+const courses = [];
+
+export function logger(reducer: ActionReducer<any>)
     : ActionReducer<any> {
     return (state, action) => {
         console.log("state before: ", state);
